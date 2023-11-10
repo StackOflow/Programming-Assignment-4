5b85ff0703807a74fd1131a6ce60c83f28137032
StackOflow <132242531+StackOflow@users.noreply.github.com>
2023-11-10 17:20:48 -0500

    Add c# console project

---
+++ Programming assignment 4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Programming_assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
